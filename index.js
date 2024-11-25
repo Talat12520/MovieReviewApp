ad485480dd79eea99a60e2a2063cd7922c4843b1
Talat12520 <saimatfirdous@gmail.com>
2024-11-25 12:17:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import app from "./server.js"
+import mongodb from "mongodb"
+import reviewsDAO from "./dao/reviewsDAO.js"
+
+const MongoClient=mongodb.MongoClient
+const mongo_username=process.env['MONGO_USERNAME']
+const mongo_password=process.env['MONGO_PASSWORD']
+const uri=`mongodb+srv://sam:mastmindblowing@cluster0.9yshl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const port=8000
+MongoClient.connect(
+  uri,
+  {
+    maxPoolSize:50,
+    wtimeoutMS:2500,
+    useNewUrlParser:true
+  })
+.catch(err => {
+  console.error(err.stack)
+  process.exit(1)
+})
+.then(async client =>{
+  await reviewsDAO.injectDB(client)
+  app.listen(port, () =>{
+    console.log(`listening on port ${port}`)
+  })
+})
